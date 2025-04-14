4fca12adb5bc28ce2c0628853c4d6bc9e05191ad
Thomas Coutts <G22C5857@ict.ru.ac.za>
2025-04-14 18:34:58 +0200

    Added the Task2 function

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,21 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+    public static List<String> Task2(List<String> InputList){
+        String CurrElement;
+
+        for (int i = 0;i<InputList.size();i++){
+            CurrElement = InputList.get(i);
+            CurrElement = CurrElement.substring(1);
+
+            if (CurrElement == ""){
+                InputList.remove(i);
+            } else{
+                InputList.set(i, CurrElement);
+            }
+        }
+        return InputList;
+    }
 }
+
+    
